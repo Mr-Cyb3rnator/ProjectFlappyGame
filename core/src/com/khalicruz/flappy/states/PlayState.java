1b858967c40d654ba972c9a343721bbb6d9bedcf
WilliamsMendez <88062787+WilliamsMendez@users.noreply.github.com>
2021-07-30 21:50:30 -0600

    BC
    coloque el fondo del juego

--- core/src/com/khalicruz/flappy/states/PlayState.java
+++ core/src/com/khalicruz/flappy/states/PlayState.java
@@ -2,6 +2,7 @@ package com.khalicruz.flappy.states;
 
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.khalicruz.flappy.flappygame;
 import com.khalicruz.flappy.sprites.Bird;
@@ -10,11 +11,14 @@ public class PlayState extends states {
 
     private Bird bird;
 
+    private Texture bg;
+
     public PlayState(GameStateManager gsm) {
 
         super(gsm);
         bird = new Bird(50,320);
         camera.setToOrtho(false, flappygame.WIDTH/2, flappygame.HEIGHT /2 );
+        bg = new Texture("bgMegamanjuego.png");
 
     }
 
@@ -36,6 +40,7 @@ public class PlayState extends states {
     protected void render(SpriteBatch spriteBatch) {
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
+        spriteBatch.draw(bg, camera.position.x - (camera.viewportWidth / 2),camera.position.y - (camera.viewportHeight / 2), flappygame.WIDTH / 2, flappygame.HEIGHT /2);
         spriteBatch.draw(bird.getBird(),bird.getPosition().x,bird.getPosition().y);
         spriteBatch.end();
     }
